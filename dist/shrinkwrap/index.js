ea5855f23258c86438cf0a73f0db06771eabd45a
Iain Reid <iainreid382@gmail.com>
2017-01-04 10:12:52 +0000

    Updated distribution

--- dist/shrinkwrap/index.js
+++ dist/shrinkwrap/index.js
@@ -1,4 +1,12 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments)).next());
+    });
+};
 /**
  * Dependencies
  */
@@ -7,14 +15,15 @@ const program = require("commander");
 const lib = require("../lib");
 function exec(program) {
     "use strict";
-    /**
-     * A verbose dependency graph to be used in the shrinkwrapping process. This graph contains all of the required
-     * modules listed within this project and recursively within it's dependencies.
-     */
-    const dependencyGraph = generateGraph(path.normalize(program["path"]));
-    dependencyGraph.copyModules(path.normalize(program["dest"]))
-        .then(() => {
-        console.log("Done!");
+    return __awaiter(this, void 0, void 0, function* () {
+        /**
+         * A verbose dependency graph to be used in the shrinkwrapping process. This graph contains all of the required
+         * modules listed within this project and recursively within it's dependencies.
+         */
+        const dependencyGraph = generateGraph(path.normalize(program["path"]));
+        yield dependencyGraph.copyModules(path.normalize(program["dest"]));
+        yield lib.writeJsonToFile(path.join(program["path"], "manifest.json"), dependencyGraph.toReadable());
+        console.log("Done");
     });
 }
 exports.exec = exec;
