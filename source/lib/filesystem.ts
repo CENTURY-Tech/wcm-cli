9d8ebcad527632afebde55f6f481ce7dc31270a6
Iain Reid <iainreid382@gmail.com>
2017-01-05 09:32:41 +0000

    Development update

--- source/lib/filesystem.ts
+++ source/lib/filesystem.ts
@@ -6,6 +6,40 @@ import * as path from "path";
 import { fileNotFound, upstreamDependencyFailure } from "./errors";
 
 /**
+ * Read the file at the supplied path asynchronously and throw an error if the file cannot be found.
+ */
+export function readFile(fullPath: string): Promise<string> {
+  "use strict";
+
+  if (!fs.existsSync(fullPath)) {
+    fileNotFound(fullPath).exit();
+  }
+
+  return new Promise((resolve): void => {
+    void fs.readFile(fullPath, "utf8", (err: Error, data: string): void => {
+      if (err) {
+        upstreamDependencyFailure("fs-extra", err).exit();
+      }
+
+      void resolve(data);
+    });
+  });
+}
+
+/**
+ * Read the file at the supplied path synchronously and throw an error if the file cannot be found.
+ */
+export function readFileSync(fullPath: string): string {
+  "use strict";
+
+  if (!fs.existsSync(fullPath)) {
+    fileNotFound(fullPath).exit();
+  }
+
+  return fs.readFileSync(fullPath, "utf8");
+}
+
+/**
  * Read and parse the file at the supplied path as JSON and throw an error if the file cannot be found.
  */
 export function readFileAsJson<T extends Object>(fullPath: string): T {
--- source/prepare/index.ts
+++ source/prepare/index.ts
@@ -3,9 +3,33 @@
  */
 import * as path from "path";
 import * as lib from "../lib";
+import * as htmlparser from "htmlparser2";
 
 export async function exec(entryPath: string): Promise<any> {
   "use strict";
 
-  console.log(entryPath)
+  for (let file in traverseImports(entryPath)) {
+    console.log(file["relpath"]);
+  }
+}
+
+/**
+ * Recursively process the link imports from the file at the supplied path and return a processed version with its
+ * relative path. 
+ */
+function* traverseImports(entryPath: string, rootPath: string = entryPath): IterableIterator<Object> {
+  "use strict";
+
+  const file: string = lib.readFileSync(entryPath);
+  const html = htmlparser
+
+  yield {
+    relpath: path.relative(rootPath, entryPath)
+  }
+
+  for (let dependency in moduleJson.dependencies) {
+    if (!currentGraph.hasDependency(dependency)) {
+      yield* traverseModule(path.join(modulePath, "..", dependency), rootPath);
+    }
+  }}
 }
